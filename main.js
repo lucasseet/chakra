dd1b29059ecca079e2c5bca0334e92a337232c09
lucasseet <79414028+lucasseet@users.noreply.github.com>
2021-04-22 23:41:57 +0800

    page transition / JS event handler

--- main.js
+++ main.js
@@ -1,4 +1,56 @@
-window.onload = function() {
-    console.log('All Linked!')
+window.onload = function () {
+  console.log("All Linked!");
 
-}
+  //Global Scope
+  let landingPagePlayNow = document.querySelector("#landing-button");
+  let landingPageDiv = document.querySelector("#landing-page");
+  let backgroundImage = document.querySelector("#background-wrapper");
+  let chakraInstruction = document.querySelector("#inner-background-wrapper");
+  let nameInput = document.querySelector("#name-input");
+  let chakraPlayNowBtn = document.querySelector("#button");
+  let gameElements = document.querySelector('#name-progress-cell')
+  let playerName = document.querySelector('#player-name')
+  let playerNameDoor = document.querySelector('#name-out-player')
+   
+
+  //Attached event handler to Landing page playnow button
+  landingPagePlayNow.onclick = (event) => {
+    event.preventDefault();
+    
+
+    //Transit landing page to chakra instruction page
+    landingPageDiv.style.display = "none";
+    backgroundImage.style.display = "block";
+    chakraInstruction.style.display = "block";
+
+    //Auto resize screen to 1350px by 800px
+    window.resizeTo(1350, 800);
+
+    //Attach event handler on ChakraPlayNow Button
+    chakraPlayNowBtn.onclick = (event) => {
+      event.preventDefault();
+
+      //Get name value, and trim away whitespaces
+      let nameVal = nameInput.value;
+      nameVal = nameVal.trim();
+      nameVal = nameVal.toUpperCase()
+
+      //perform validation
+      if (nameVal === "") {
+        alert("Please enter your name");
+        return;
+      }
+
+      //Transit from ChakraInstruction to GameMap
+      chakraInstruction.style.display = "none";
+      gameElements.style.display = "block"
+
+      //Insert nameValue onto healthbar & door [IF IT HAS LONG NAME IT DOESNT CENTRALIZE]
+      playerName.innerText = nameVal
+      playerNameDoor.innerText = nameVal
+
+
+
+    };
+  };
+};
