e5078340866f193ccdc643baf8fbee886c2ae97c
lucasseet <79414028+lucasseet@users.noreply.github.com>
2021-04-24 17:58:45 +0800

    Distribute card + logic on win conditions

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//Chakra Elements
+let cardElements = ['fire', 'water', 'wind', 'earth', 'lightning', 'health']
+console.log(cardElements)
+
 window.onload = function () {
   console.log("All Linked!");
 
@@ -11,7 +15,10 @@ window.onload = function () {
   let gameElements = document.querySelector('#name-progress-cell')
   let playerName = document.querySelector('#player-name')
   let playerNameDoor = document.querySelector('#name-out-player')
-   
+  let comCells = document.querySelectorAll('.com-cell')
+  let comDeck = document.querySelectorAll('.com-deck')
+  let playerCells = document.querySelectorAll('.player-cell')
+  let playerDeck = document.querySelectorAll('.player-deck')
 
   //Attached event handler to Landing page playnow button
   landingPagePlayNow.onclick = (event) => {
@@ -24,8 +31,8 @@ window.onload = function () {
     chakraInstruction.style.display = "block";
 
     //Auto resize screen to 1350px by 800px
-    window.resizeTo(1350, 800);
-
+    window.resizeTo(500, 500);
+  
     //Attach event handler on ChakraPlayNow Button
     chakraPlayNowBtn.onclick = (event) => {
       event.preventDefault();
@@ -50,7 +57,70 @@ window.onload = function () {
       playerNameDoor.innerText = nameVal
 
 
+      // Start Game, Distribute cards
+      
+      //Create empty Player & Com array
+      const playerDeckArray = []
+      const comDeckArray = []
+
+      //Create a deck with random element pushing it into player deck
+      for (let i = 0; i < playerCells.length; i++) {
+        playerDeckArray.push(randomElement())
+        console.log(playerDeckArray)
+      }
+
+      //Create a deck with random element pushing it into com deck
+      for (let i = 0; i < comCells.length; i++) {
+        comDeckArray.push(randomElement())
+        console.log(comDeckArray)
+      }
+
+      //Make player cards appear in DOM
+      playerCells.forEach((card, index) => {
+        const element = playerDeckArray[index]
+        card.classList.add(element)
+        card.addEventListener('click', () => handleSelect(element))
+        console.log(`Clicked ${element}`)
+      })
+
+
+  
 
     };
   };
 };
+
+//Get random element -> generate random number btn 0 & length of element
+function randomElement(){
+  let randomNum = Math.floor(Math.random() * (cardElements.length -1))
+  return cardElements[randomNum]
+}
+
+//Making a move function
+function makeMove(event) {
+    event.stopPropagation()
+    console.log('making a move now!')
+}
+// Player Clicks 
+function handleSelect() {
+  let comCard = enemyMove ()
+  console.log(comCard)
+}
+
+// Enemy move
+function enemyMove() {
+  let comDeck = document.querySelectorAll('.com-deck')
+  const randomIndex = generateRandomIndex()
+  const cardElement = comDeck[randomIndex]
+}
+
+// function that returns a number between 0 to 3
+function generateRandomIndex() {
+  let randomNum = Math.floor(Math.random() * 3)
+  console.log(randomNum)
+}
+
+// function playerWins(playerCard, comCard) {
+
+// }
+
